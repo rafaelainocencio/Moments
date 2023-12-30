e634c7eb02d9c7abe770b511524bc5eac0ec45c9
rafaela montalvao <rafaelainocencioo@gmail.com>
2023-12-30 20:50:06 -0300

    Finalizando mensageria e página about

--- moments/src/app/components/pages/new-moment/new-moment.component.ts
+++ moments/src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { Moment } from 'src/Interfaces/Moment';
+import { Router } from '@angular/router';
 
 import { MomentService } from 'src/app/services/moment.service';
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-new-moment',
@@ -11,7 +13,10 @@ import { MomentService } from 'src/app/services/moment.service';
 export class NewMomentComponent {
   btnText = "Compartilhar!"
 
-  constructor(private momentService: MomentService) { }
+  constructor(
+    private momentService: MomentService, 
+    private messageService: MessageService,
+    private router: Router) { }
 
   //recebe os dados (que vem do evento) do componente filho
   async createHandler(moment: Moment) {
@@ -30,7 +35,9 @@ export class NewMomentComponent {
       this.momentService.createMoment(formData).subscribe();
 
       //exibir mensagem
+      this.messageService.add('Momento adicionado com sucesso!');
 
       //redirect
+      this.router.navigate(['/']);
   }
 }
--- moments/src/app/services/message.service.ts
+++ moments/src/app/services/message.service.ts
@@ -5,5 +5,18 @@ import { Injectable } from '@angular/core';
 })
 export class MessageService {
   message: string = '';
+
   constructor() { }
+
+  add(message: string) {
+    this.message = message;
+
+    setTimeout(() => {
+     
+    }, 4000);
+  }
+
+  clear() {
+    this.message = '';
+  }
 }
